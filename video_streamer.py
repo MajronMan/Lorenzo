adb87dd0961ef30a2890ecd44aa2d0d59e1f46f5
AdamRobertsky <robertadamski96@gmail.com>
2017-11-25 16:16:21 +0100

    Video streamer

--- video_streamer.py
+++ video_streamer.py
@@ -1,2 +1,17 @@
+import cv2
+import time
+
+capture = cv2.VideoCapture(0)
 def read_stream():
-    pass
+    return capture.read()[1]
+
+def read_period(period):
+    t0 = time.time()
+    res = []
+    while time.time() - t0 < period:
+        res.append(read_stream())
+    return res
+
+def quit_video():
+    capture.release()
+    cv2.destroyAllWindows()
