43b4629faef0e1cfbfe377effa2b3f047883f002
Miron Markowski <majron15@gmail.com>
2017-11-25 15:58:50 +0100

    Audio player

--- audio_player.py
+++ audio_player.py
@@ -1,2 +1,35 @@
-def play_audio(audio_data):
-    pass
+import pygame
+import pygame.midi
+
+
+class Player:
+    def init(self):
+        self.active_note = None
+        pygame.midi.init()
+        self.player = pygame.midi.Output(1)
+        self.player.set_instrument(0, 0)
+
+    def play(self, pitch, volume):
+        if self.active_note[0] != pitch:
+            self.player.note_off(self.active_note[0], self.active_note[1])
+            self.player.note_on(pitch, volume)
+            self.active_note = (pitch, volume)
+
+    def quit(self):
+        del self.player
+        pygame.midi.quit()
+
+
+player = Player()
+
+
+def play_audio(pitch, volume):
+    player.play(pitch, volume)
+
+
+def init_audio():
+    player.init()
+
+
+def quit_audio():
+    player.quit()
--- auralizer.py
+++ auralizer.py
@@ -1,4 +1,3 @@
-import wave
 import random
 import struct
 import numpy as np
@@ -6,8 +5,6 @@ import numpy as np
 SAMPLE_LEN = 1024
 
 def fake_auralize(video_data):
-    noise_output = wave.open('noise.wav', 'w')
-    noise_output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
     noise = np.zeros(SAMPLE_LEN)
 
     for i in range(0, SAMPLE_LEN):
