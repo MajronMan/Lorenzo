59da12010d7bc44fad2879504e89fb32f3727531
AdamRobertsky <robertadamski96@gmail.com>
2017-11-25 23:51:28 +0100

    'Sweatings'

--- audio_player.py
+++ audio_player.py
@@ -6,8 +6,13 @@ class Player:
     def init(self):
         self.active_note = None
         pygame.midi.init()
+<<<<<<< Updated upstream
         self.player = pygame.midi.Output(1)
         self.player.set_instrument(17)
+=======
+        self.player = pygame.midi.Output(0)
+        self.player.set_instrument(0, 0)
+>>>>>>> Stashed changes
 
     def play(self, pitch, volume):
         if self.active_note is None:
