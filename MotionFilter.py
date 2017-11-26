84c06431784e380c0ab6774430fbc91bc4bd9710
luknw <luknwpl@gmail.com>
2017-11-26 09:21:17 +0100

    Remove glitter

--- MotionFilter.py
+++ MotionFilter.py
@@ -1,33 +1,34 @@
 import cv2
-import imutils
 
-from utils import show
 from ImageFilter import ImageFilter
 
 
 class MotionFilter(ImageFilter):
-    def __init__(self, max_background_age=10, min_contour_area=1000, max_contour_area=2000):
+    def __init__(self, max_background_age=1, min_contour_area=1000):
         self.max_background_age = max_background_age
         self.background_age = max_background_age + 1
-        self.background_frame = None
         self.min_contour_area = min_contour_area
-        self.max_contour_area = max_contour_area
+        self.background_frame = None
+        self.last_frame = None
 
     def filter(self, frame):
-        preprocessed = self.preprocess_frame(frame)
+        current = self.preprocess_frame(frame)
 
         if self.background_age > self.max_background_age:
-            self.background_frame = preprocessed
+            self.background_frame = current
             self.background_age = 0
+            if self.last_frame is not None:
+                current = self.last_frame  # prevent comparing background with itself
 
+        self.last_frame = current
         self.background_age += 1
 
-        frame_delta = cv2.absdiff(self.background_frame, preprocessed)
+        frame_delta = cv2.absdiff(self.background_frame, current)
         # show(frame_delta, 'delta')
         result = cv2.threshold(frame_delta, 20, 255, cv2.THRESH_BINARY)[1]
         # show(result, 'threshold')
 
-        result = cv2.dilate(result, None, iterations=8)
+        result = cv2.dilate(result, None, iterations=0)
         # show(result, 'dilated')
         img, contours, _ = cv2.findContours(result.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -39,7 +40,7 @@ class MotionFilter(ImageFilter):
         return result
 
     def preprocess_frame(self, frame):
-        frame = imutils.resize(frame, width=500)
+        # frame = imutils.resize(frame, width=500)
         # show(frame, 'resized')
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # show(frame, 'grayscale')
